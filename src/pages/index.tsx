b9915c83fdcaea7e3b98478acc9c263e935ecc5a
Filipe Motta <femotta96@gmail.com>
2022-09-21 18:46:03 -0300

    Estrutura visual: Configurando Stitches

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,21 @@
+import { styled } from "../styles";
+
+const Button = styled("button", {
+  backgroundColor: "$rocketseat",
+  borderRadius: 8,
+  border: 0,
+  padding: "4px 8px",
+
+  span: {
+    fontWeight: "bold",
+  },
+});
+
 export default function Home() {
-  return <h1>Hello World</h1>;
+  return (
+    <Button>
+      <span>Teste</span>
+      Enviar
+    </Button>
+  );
 }
