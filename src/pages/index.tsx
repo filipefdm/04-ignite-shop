534e0b15774fcfcc9f9373bceb4611070d14fce9
filipefdm <femotta96@gmail.com>
2022-10-07 14:58:00 -0300

    :sparkles: feat: making a few changes because of contexts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,31 +3,31 @@ import Head from "next/head";
 import Image from "next/future/image";
 import Link from "next/link";
 
-import { useKeenSlider } from "keen-slider/react";
+import useEmblaCarousel from "embla-carousel-react";
 
 import { stripe } from "../lib/stripe";
-
-import { HomeContainer, Product } from "../styles/pages/home";
-
-import "keen-slider/keen-slider.min.css";
 import Stripe from "stripe";
 
+import { HomeContainer, Product, SliderContainer } from "../styles/pages/home";
+
+import { CartButton } from "../components/CartButton";
+
+import { useCart } from "../hooks/useCart";
+
+import { IProduct } from "../contexts/CartContext";
+
 interface HomeProps {
-  products: {
-    id: string;
-    name: string;
-    imageUrl: string;
-    price: string;
-  }[];
+  products: IProduct[];
 }
 
 export default function Home({ products }: HomeProps) {
-  const [sliderRef] = useKeenSlider({
-    slides: {
-      perView: 3,
-      spacing: 48,
-    },
+  const [emblaRef] = useEmblaCarousel({
+    align: "start",
+    skipSnaps: false,
+    dragFree: true,
   });
+
+  const { addToCart } = useCart();
 
   return (
     <>
@@ -35,26 +35,44 @@ export default function Home({ products }: HomeProps) {
         <title>Home | Ignite Shop</title>
       </Head>
 
-      <HomeContainer ref={sliderRef} className="keen-slider">
-        {products.map((product) => {
-          return (
-            <Link
-              href={`/product/${product.id}`}
-              key={product.id}
-              prefetch={false}
-            >
-              <Product className="keen-slider__slide">
-                <Image src={product.imageUrl} width={520} height={480} alt="" />
+      <div style={{ overflow: "hidden", width: "100%" }}>
+        <HomeContainer>
+          <div className="embla" ref={emblaRef}>
+            <SliderContainer className="embla__container container">
+              {products.map((product) => {
+                return (
+                  <Link
+                    href={`/product/${product.id}`}
+                    key={product.id}
+                    prefetch={false}
+                  >
+                    <Product className="embla__slide">
+                      <Image
+                        src={product.imageUrl}
+                        width={520}
+                        height={480}
+                        alt=""
+                      />
 
-                <footer>
-                  <strong>{product.name}</strong>
-                  <span>{product.price}</span>
-                </footer>
-              </Product>
-            </Link>
-          );
-        })}
-      </HomeContainer>
+                      <footer>
+                        <div>
+                          <strong>{product.name}</strong>
+                          <span>{product.price}</span>
+                        </div>
+                        <CartButton
+                          color="green"
+                          size="large"
+                          onClick={() => addToCart(product)}
+                        />
+                      </footer>
+                    </Product>
+                  </Link>
+                );
+              })}
+            </SliderContainer>
+          </div>
+        </HomeContainer>
+      </div>
     </>
   );
 }
