d0f3d0c247dd1b8b38c1bda8356dbb1c653ccba6
filipefdm <femotta96@gmail.com>
2022-10-07 14:59:40 -0300

    :sparkles: feat: add button & a properties

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -5,13 +5,23 @@ export const globalStyles = globalCss({
     margin: 0,
     padding: 0,
   },
+
   body: {
     backgroundColor: "$gray900",
     color: "$gray100",
     "-webkit-font-smoothing": "antialiased",
   },
+
   "body, input, textarea, button": {
     fontFamily: "Roboto",
     fontWeight: 400,
+  },
+
+  a: {
+    color: "inherit",
+  },
+
+  button: {
+    cursor: "pointer",
   },
 });
