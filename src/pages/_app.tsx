1f7229e849862a9071b9daf1c3eb82f5141cdce4
filipefdm <femotta96@gmail.com>
2022-10-07 14:57:37 -0300

    :sparkles: feat: add some changes to app component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,38 @@
 import { AppProps } from "next/app";
+import { Header } from "../components/Header";
+import { useState, useEffect } from "react";
 import { globalStyles } from "../styles/global";
 
-import logoImg from "../assets/logo.svg";
-import { Container, Header } from "../styles/pages/app";
-import Image from "next/future/image";
+import { Container } from "../styles/pages/app";
+
+import { CartContextProvider } from "../contexts/CartContext";
+
+// import dynamic from "next/dynamic";
 
 globalStyles();
 
+// const dynamicHeader = dynamic(() => import("../components/Header/index"), {
+//   ssr: false,
+// });
+
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <Container>
-      <Header>
-        <Image src={logoImg} alt="logo" />
-      </Header>
-      <Component {...pageProps} />
-    </Container>
-  );
+  const [showChild, setShowChild] = useState(false);
+  useEffect(() => {
+    setShowChild(true);
+  }, []);
+  if (!showChild) {
+    return null;
+  }
+  if (typeof window === "undefined") {
+    return <></>;
+  } else {
+    return (
+      <CartContextProvider>
+        <Container>
+          <Header />
+          <Component {...pageProps} />
+        </Container>
+      </CartContextProvider>
+    );
+  }
 }
