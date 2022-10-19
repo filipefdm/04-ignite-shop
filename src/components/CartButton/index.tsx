3425230c99daf633c3e954de67c1cee27cade60d
Filipe Motta <femotta96@gmail.com>
2022-10-19 19:12:12 -0300

    :sparkles: feat: add quantity property to CartButtonProps interface and make it dynamic into the cart button container

--- src/components/CartButton/index.tsx
+++ src/components/CartButton/index.tsx
@@ -2,11 +2,14 @@ import { ComponentProps } from "@stitches/react";
 import { Handbag } from "phosphor-react";
 import { CartButtonContainer } from "./styles";
 
-type CartButtonProps = ComponentProps<typeof CartButtonContainer>;
+type CartButtonProps = ComponentProps<typeof CartButtonContainer> & {
+  quantity?: number;
+};
 
-export function CartButton({ ...rest }: CartButtonProps) {
+export function CartButton({ quantity = 0, ...rest }: CartButtonProps) {
   return (
     <CartButtonContainer {...rest}>
+      {quantity > 0 && <span>{quantity}</span>}
       <Handbag weight="bold" />
     </CartButtonContainer>
   );
