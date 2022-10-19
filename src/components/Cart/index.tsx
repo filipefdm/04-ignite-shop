6f03be1a252fd78fb4c150c76169321844334f44
Filipe Motta <femotta96@gmail.com>
2022-10-19 19:10:05 -0300

    :sparkles: feat: add quantity property to show cart quantity items

--- src/components/Cart/index.tsx
+++ src/components/Cart/index.tsx
@@ -47,7 +47,7 @@ export function Cart() {
   return (
     <Dialog.Root>
       <Dialog.Trigger asChild>
-        <CartButton />
+        <CartButton quantity={cartQuantity} />
       </Dialog.Trigger>
 
       <Dialog.Portal>
