d602c2b6f1b09e9ebc10b5fa20403fd42caae800
Filipe Motta <femotta96@gmail.com>
2022-10-19 18:31:41 -0300

    :bug: fix: providing API key

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -1,8 +1,11 @@
 import Stripe from "stripe";
 
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-  apiVersion: "2022-08-01",
-  appInfo: {
-    name: "Ignite Shop",
-  },
-});
+export const stripe = new Stripe(
+  "sk_test_51LkmicDEVJAJOXAylPBk88qZC9iQ17OJsaMiTjlhTHA6m3wlowhf5MZjepUn0xf8YlaIL4k3R3KrEFKHJSZlfZ1q00RcAQJXse",
+  {
+    apiVersion: "2022-08-01",
+    appInfo: {
+      name: "Ignite Shop",
+    },
+  }
+);
