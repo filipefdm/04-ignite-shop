8b5e12aafe3507eedac6db14aba7fd2e81417279
Filipe Motta <femotta96@gmail.com>
2022-10-19 17:39:54 -0300

    :recycle: refactor: create dynamic routes condition

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -5,9 +5,13 @@ import logoImg from "../../assets/logo.svg";
 import Image from "next/future/image";
 import Link from "next/link";
 import { Cart } from "../Cart";
-
+import { useRouter } from "next/router";
 
 export function Header() {
+  const { pathname } = useRouter();
+
+  const showCartButton = pathname !== "/success";
+
   return (
     <HeaderContainer>
       <Link href="/">
@@ -16,7 +20,7 @@ export function Header() {
         </a>
       </Link>
 
-      <Cart />
+      {showCartButton && <Cart />}
     </HeaderContainer>
   );
 }
